105c51fc3db6ac070c05404d5aa306bc79217a86
Asare <opokuasarekennedy@gmail.com>
2024-09-11 20:09:16 +0300

    Fix double jobs running.

--- Build_Operate/diabetes-training.py
+++ Build_Operate/diabetes-training.py
@@ -1,6 +1,7 @@
 # import libraries
 from azureml.core import Run
 import joblib
+import argparse
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -10,6 +11,12 @@ from sklearn.metrics import roc_curve
 
 # Get the experiment run context 
 run = Run.get_context()
+
+# Set regularization hyperparameter
+parser = argparse.ArgumentParser()
+parser.add_argument('--reg-rate', type=float, dest='reg_rate', default=0.01)
+args = parser.parse_args()
+reg = args.reg_rate
 
 # load the diabetes dataset
 print("Loading Data...")
@@ -22,9 +29,6 @@ y = diabetes['Diabetic'].values
 # split data into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
-# set regularization hyperparameter
-reg = 0.01
-
 # train a logistic regression model
 print('Training a logistic regression model with regularization rate of', reg)
 model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
@@ -33,13 +37,13 @@ model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
 y_hat = model.predict(X_test)
 acc = np.average(y_hat == y_test)
 print('Accuracy:', float(acc))
-run.log('Accuracy:', float(acc))
+run.log('Accuracy', float(acc))
 
 # calculate AUC
 y_scores = model.predict_proba(X_test)
 auc = roc_auc_score(y_test,y_scores[:,1])
 print('AUC: ' + str(auc))
-run.log('AUC: ', float(auc))
+run.log('AUC', float(auc))
 
 # Save the model
 os.makedirs('outputs', exist_ok=True)
